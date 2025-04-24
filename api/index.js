07dfad85a1848373fb11c31b926e1d5d415081f2
Karan7385 <karanvishwakarma7385@gmail.com>
2025-04-24 21:15:32 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const authRoutes = require('./routes/authRoutes');
-const { connectDB } = require('./config/db');
+const authRoutes = require('../routes/authRoutes');
+const { connectDB } = require('../config/db');
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
